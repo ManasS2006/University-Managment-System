8c0117c6445c051a2cc24c5babce825cb442410f
Michelle <michellele6055@gmail.com>
2025-03-02 16:22:34 -0500

    idk im testing

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testing");
         System.out.println("money");
-        
+        System.out.println("Another test");
     }
 }
